94ec450beac164c0e6d8a04daf654145741189ea
JesusCotaV <86278387+JesusCotaV@users.noreply.github.com>
2023-07-05 01:35:47 -0700

    feat: version de api con cors que acepta cualquier origen

--- API REST/Program.cs
+++ API REST/Program.cs
@@ -11,8 +11,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:4200",
-                                             "http://localhost:34255")
+                          policy.WithOrigins("*")
                                 .AllowAnyHeader()
                                 .AllowAnyMethod();
                       });
